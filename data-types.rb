de9fcb81a5033484cddef677d9cb4933ea3f78e3
Sharmin <sharmin@Sharmins-MacBook-Pro.local>
2019-04-12 22:05:27 +0600

    Use of datatypes

---
+++ data-types.rb
@@ -0,0 +1,12 @@
+phrase = "sharmin Akter"
+name = "IRiN"
+
+puts phrase.upcase()
+puts name.downcase()
+
+puts phrase[0]
+puts phrase.include? "Akter"
+puts phrase[0,3]
+puts phrase.length()
+puts name.include? "ir"
+puts name.include? "IR"
