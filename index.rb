d5cf35ad3dd45e8bd79f31538463e5462db61d3e
Sharmin <sharmin@Sharmins-MacBook-Pro.local>
2019-04-12 16:56:03 +0600

    second commit

--- index.rb
+++ index.rb
@@ -1,1 +1,10 @@
-print "Hello world"
+print "I want to learn"
+puts "Ruby"
+
+puts "I want to learn"
+
+
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
---
+++ variables.rb
@@ -0,0 +1,6 @@
+character_name = "Mahin"
+character_age = "20"
+
+puts ("I am "+character_name)
+puts "I am very lazy student."
+puts ("I am "+ character_age + " years old.")
