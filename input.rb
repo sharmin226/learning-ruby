b368d48515818f21462032acf2de1d99e9f513fe
Sharmin <sharmin@Sharmins-MacBook-Pro.local>
2019-04-13 01:14:17 +0600

    User input

---
+++ input.rb
@@ -0,0 +1,9 @@
+puts "Enter your name:"
+name = gets
+puts ("I am " + name + "You are awesome")
+
+puts "Enter your name:"
+name = gets.chomp()
+puts "Enter your age:"
+age = gets.chomp()
+puts ("I am " + name + ". I am " + age + " years old.")
