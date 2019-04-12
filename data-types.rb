d2bbcf81427f621160566028ee8d410cb123bf65
Sharmin <sharmin@Sharmins-MacBook-Pro.local>
2019-04-12 22:30:49 +0600

    Mathematical calculation

--- data-types.rb
+++ data-types.rb
@@ -10,3 +10,51 @@ puts phrase[0,3]
 puts phrase.length()
 puts name.include? "ir"
 puts name.include? "IR"
+
+a = 123
+puts a
+
+b = 9.076
+puts b
+
+puts phrase.index("A")
+puts phrase.index("h")
+
+puts 2/3
+puts 24/3
+puts 3*4
+
+puts 2**3
+puts 2**4
+
+puts 10%3
+
+num = -20
+puts num.abs()
+
+n = 20.889
+puts n.round()
+m = 10.234
+puts m.round()
+
+number = 89
+puts ("The real number is " +  number.to_s)
+
+k = 56.2
+puts ("The number is " + k.to_s)
+
+puts k.ceil()
+puts k.floor()
+
+z = 16
+puts Math.sqrt(z)
+y =40
+puts Math.sqrt(y)
+puts Math.log(y)
+puts Math.log(1)
+
+puts 1+2
+puts 190+879
+puts 1.2+3
+puts 12.4/2
+puts 13/2
