9ac22c507b6fc7e9767acb72a642012a7394cb81
Sharmin <sharmin@Sharmins-MacBook-Pro.local>
2019-04-13 11:13:29 +0600

    madlibs game

---
+++ madlibs.rb
@@ -0,0 +1,11 @@
+puts "Enter an adjective:"
+adj = gets.chomp()
+puts "Enter a verb"
+verb = gets.chomp()
+puts "Enter a pronoun"
+pro = gets.chomp()
+
+
+puts ("Life is challenging but " + adj)
+puts (verb + " every moment of your life")
+puts ("Love " + pro)
