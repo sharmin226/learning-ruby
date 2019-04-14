c8fb0e3dff277f9c4f431c47304ce5d4db47046f
Sharmin <sharmin@Sharmins-MacBook-Pro.local>
2019-04-14 22:30:17 +0600

    for loop

---
+++ forloop.rb
@@ -0,0 +1,35 @@
+friends = ["mina", "ranu", "lenin", "jerin"]
+
+for element in friends
+    puts element
+end
+
+
+
+
+
+#names = ['Foo', 'Bar', 'Baz']
+names = ["Foo", "Bar", "Baz"]
+
+
+names.each { |item|
+    puts item
+}
+puts
+
+names.each do |item|
+    puts item
+end
+
+puts
+
+
+for index in 0..5
+  puts index
+end
+
+puts
+
+6.times do|index|
+  puts index
+end
