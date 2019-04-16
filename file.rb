4b81c2340ee5ced3239d9b5462ba1431eb19269d
Sharmin <sharmin@Sharmins-MacBook-Pro.local>
2019-04-16 21:56:02 +0600

    file read

---
+++ file.rb
@@ -0,0 +1,15 @@
+file = File.open("member.txt", "r") #do |file|
+
+  #puts file
+  #puts file.read()
+  #puts
+  #puts file.read().include? "adsdfgdfn"
+  #puts file.read().include? "irin"
+  #puts file.readline()
+  #puts file.readline()
+  #puts file.readlines()
+  puts file.readchar()
+
+
+#file.close()
+#end
