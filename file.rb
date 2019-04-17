b4c2ecad2fb0908f2a50fc8f47861f61f54e11ab
Sharmin <sharmin@Sharmins-MacBook-Pro.local>
2019-04-17 13:29:37 +0600

    file write

--- file.rb
+++ file.rb
@@ -1,4 +1,4 @@
-file = File.open("member.txt", "r") #do |file|
+#file = File.open("member.txt", "r") #do |file|
 
   #puts file
   #puts file.read()
@@ -8,8 +8,32 @@ file = File.open("member.txt", "r") #do |file|
   #puts file.readline()
   #puts file.readline()
   #puts file.readlines()
-  puts file.readchar()
+  #puts file.readlines()[2]
+  #puts file.readchar()
+
+=begin
+for line in file.readlines()
+  puts line
+end
 
 
-#file.close()
+puts file.read
+file.close()
+=end
+
+
 #end
+
+#file = File.open("member.txt", "a")
+#  file.write("\nToma, Teacher")
+
+#File.open("index.html","w") do |file|
+#  file.write("<h1>Hello</h1>")
+#end
+
+
+file = File.open("member.txt", "r+")
+  #file.readline() next line cursor
+  #file.readchar() next char first line
+
+  #file.write("NEW") overwrite second 3 letters only
